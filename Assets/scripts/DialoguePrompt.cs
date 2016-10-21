63dfa20a15d4a11bbb3a804ae83cbebaa7107624
Juan Rodríguez <timrodz@icloud.com>
2016-10-22 10:37:03 +1300

    Add UIAnimation class, iTween plugin, update VR Room

--- Assets/scripts/DialoguePrompt.cs
+++ Assets/scripts/DialoguePrompt.cs
@@ -23,15 +23,25 @@ public class DialoguePrompt : MonoBehaviour {
 				
 				// Make the dialogue box visible
 				if (!hasInteractedWithDialogue) {
-					
-					StartCoroutine(dialogueScript.FadeIn());
+
+					iTween.MoveBy(promptCanvasGroup.gameObject, 
+						iTween.Hash("y", -1.2, "easeType", "easeInOutExpo", "loopType", "none", "delay", 0.1)
+						);
+
+					StartCoroutine(UIAnimation.FadeIn(containerCanvasGroup, 1));
+					//StartCoroutine(dialogueScript.FadeIn());
 					hasInteractedWithDialogue = true;
 
 				}
 				// Make the dialogue box invisible
 				else {
 
-					StartCoroutine(dialogueScript.FadeOut());
+					iTween.MoveBy(promptCanvasGroup.gameObject,
+						iTween.Hash("y", 1.2, "easeType", "easeInOutExpo", "loopType", "none", "delay", 0.1)
+						);
+
+					StartCoroutine(UIAnimation.FadeOut(containerCanvasGroup, 1));
+					//StartCoroutine(dialogueScript.FadeOut());
 					hasInteractedWithDialogue = false;
 
 				}
@@ -52,9 +62,10 @@ public class DialoguePrompt : MonoBehaviour {
 
 			promptCanvasGroup = dialogueBoxCanvasGroups[0];
 			containerCanvasGroup = dialogueBoxCanvasGroups[1];
-			dialogueScript = other.GetComponentInChildren<DialogueEnter>();
+			//dialogueScript = other.GetComponentInChildren<DialogueEnter>();
 
-			promptCanvasGroup.alpha = 1;
+			StartCoroutine(UIAnimation.FadeIn(promptCanvasGroup, 1));
+			//promptCanvasGroup.alpha = 1;
 
 			if (containerCanvasGroup.alpha == 1) {
 				hasInteractedWithDialogue = true;
@@ -76,10 +87,11 @@ public class DialoguePrompt : MonoBehaviour {
 		if (other.tag == "Dialogue") {
 
 			// Reset the variable states
-			promptCanvasGroup.alpha = 0;
-			promptCanvasGroup = null;
+			StartCoroutine(UIAnimation.FadeOut(promptCanvasGroup, 1));
+			//promptCanvasGroup.alpha = 0;
+			//promptCanvasGroup = null;
 			containerCanvasGroup = null;
-			dialogueScript = null;
+			//dialogueScript = null;
 			hasInteractedWithDialogue = false;
 			canInteractWithDialogue = false;
 			
--- Assets/scripts/UIAnimation.cs
+++ Assets/scripts/UIAnimation.cs
@@ -1,15 +1,99 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using VR = UnityEngine.VR;
 
 public class UIAnimation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public animationType AnimationType;
+
+	public enum animationType {
+		FadeIn,
+		FadeOut,
+		TextIn,
+		TextOut
+	};
+
+	public void Animate(animationType _at) {
+
+		switch (_at) {
+			case animationType.FadeIn:
+				break;
+			case animationType.FadeOut:
+				break;
+			case animationType.TextIn:
+				break;
+			case animationType.TextOut:
+				break;
+			default:
+				break;
+		}
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	/// <summary>
+	/// Fades the canvas group in
+	/// </summary>
+	public static IEnumerator FadeIn(CanvasGroup canvas, float fadeTime) {
+
+		for (float t = 0.0f; t < 1.0f; t += (Time.deltaTime / fadeTime)) {
+
+			canvas.alpha = t;
+			yield return null;
+
+		}
+
+		canvas.alpha = 1;
+		//StartCoroutine(TextIn());
+
 	}
+
+	/// <summary>
+	/// Fades the canvas group out
+	/// </summary>
+	public static IEnumerator FadeOut(CanvasGroup canvas, float fadeTime) {
+
+		for (float t = 1.0f; t > 0.0f; t -= (Time.deltaTime / fadeTime)) {
+
+			canvas.alpha = t;
+			yield return null;
+
+		}
+
+		canvas.alpha = 0;
+
+		canvas = null;
+
+	}
+
+	/// <summary>
+	/// Shows the text in a typewriter style
+	/// </summary>
+	public static IEnumerator TextIn(Text dialogueBox, string strText, float textDelay) {
+
+		for (int i = 0; i <= strText.Length; i++) {
+
+			dialogueBox.text = strText.Substring(0, i);
+			yield return new WaitForSeconds(textDelay);
+
+		}
+
+	}
+
+	/// <summary>
+	/// Removes the text in a typewriter style
+	/// </summary>
+	public static IEnumerator TextOut(Text dialogueBox, string strText, float textDelay) {
+
+		for (int i = strText.Length; i >= 0; i--) {
+
+			dialogueBox.text = strText.Substring(0, i);
+			yield return new WaitForSeconds(textDelay);
+
+		}
+
+		//StartCoroutine(FadeOut());
+
+	}
+
 }
